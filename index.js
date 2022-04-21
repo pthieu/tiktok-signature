69adf50573b2fe6db0ec8181dc57386e73ad935d
Phong Thieu <pthieu@gmail.com>
2022-04-21 02:57:41 -0400

    Swap over to chrome-aws-lambda and puppeter-core

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { createCipheriv } = require("crypto");
-const { devices, chromium } = require("playwright-chromium");
+const url = require("url");
+const chromium = require('chrome-aws-lambda');
 const Utils = require("./utils");
-const iPhone11 = devices["iPhone 11 Pro"];
+const iPhone11 = {
+  userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Mobile/15E148 Safari/604.1',
+  viewport: { width: 375, height: 812 },
+  deviceScaleFactor: 3,
+  isMobile: true,
+  hasTouch: true,
+  defaultBrowserType: 'webkit'
+}
+  
 class Signer {
   userAgent =
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36";
@@ -43,35 +52,38 @@ class Signer {
 
   async init() {
     if (!this.browser) {
-      this.browser = await chromium.launch(this.options);
+      const execPath = await chromium.executablePath;
+      const options = {
+        ...this.options,
+        executablePath: execPath,
+      }
+      this.browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      });
     }
 
-    let emulateTemplate = {
-      ...iPhone11,
-      locale: "en-US",
-      deviceScaleFactor: Utils.getRandomInt(1, 3),
-      isMobile: Math.random() > 0.5,
-      hasTouch: Math.random() > 0.5,
-      userAgent: this.userAgent,
-    };
-    emulateTemplate.viewport.width = Utils.getRandomInt(320, 1920);
-    emulateTemplate.viewport.height = Utils.getRandomInt(320, 1920);
+    // let emulateTemplate = {
+    //   ...iPhone11,
+    //   locale: "en-US",
+    //   deviceScaleFactor: Utils.getRandomInt(1, 3),
+    //   isMobile: Math.random() > 0.5,
+    //   hasTouch: Math.random() > 0.5,
+    //   userAgent: this.userAgent,
+    // };
+    // emulateTemplate.viewport.width = Utils.getRandomInt(320, 1920);
+    // emulateTemplate.viewport.height = Utils.getRandomInt(320, 1920);
 
-    this.context = await this.browser.newContext({
-      ...emulateTemplate,
-    });
+    // this.context = await this.browser.newContext({
+    //   ...emulateTemplate,
+    // });
 
-    this.page = await this.context.newPage();
-
-    await this.page.route("**/*", (route) => {
-      return route.request().resourceType() === "script"
-        ? route.abort()
-        : route.continue();
-    });
-
-    await this.page.goto(this.default_url, {
-      waitUntil: "networkidle",
-    });
+    // this.context = await this.browser.createIncognitoBrowserContext();
+    this.page = await this.browser.newPage();
+    await this.page.goto(this.default_url);
 
     let LOAD_SCRIPTS = ["signer.js", "webmssdk.js"];
     LOAD_SCRIPTS.forEach(async (script) => {
